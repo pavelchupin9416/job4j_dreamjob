dfda44cef3609c07f067a26d846e7c050c962df4
PavelChupin <pavelchupin94@yandex.ru>
2023-11-29 16:12:36 +0300

    5. Список кандидатов [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -0,0 +1,64 @@
+package ru.job4j.dreamjob.repository;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryCandidateRepository implements  CandidateRepository {
+    private static final  MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
+    private int nextId = 1;
+    private final Map<Integer, Candidate> candidates = new HashMap<>();
+
+
+    private MemoryCandidateRepository() {
+        save(new Candidate(0, "Фролов Петр Иванович", "Intern Java Developer",
+                LocalDateTime.of(2023, Month.NOVEMBER, 1, 11, 25)));
+        save(new Candidate(1, "Белугин Сергей Андреевич", "Junior Java Developer",
+                LocalDateTime.of(2023, Month.OCTOBER, 2, 15, 36)));
+        save(new Candidate(2, "Дмитрий Анисимов Петрович", "Junior+ Java Developer",
+                LocalDateTime.of(2023, Month.SEPTEMBER, 6, 12, 30)));
+        save(new Candidate(3, "Силкин Евгений Сергеевич", "Middle Java Developer",
+                LocalDateTime.of(2023, Month.AUGUST, 16, 17, 46)));
+        save(new Candidate(4, "Савельев Николай Александрович", "Middle+ Java Developer",
+                LocalDateTime.of(2023, Month.NOVEMBER, 8, 11, 12)));
+        save(new Candidate(5, "Цапин Антон Олегович", "Senior Java Developer",
+                LocalDateTime.of(2023, Month.APRIL, 15, 7, 13)));
+    }
+
+    public static MemoryCandidateRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Candidate save(Candidate candidate) {
+        candidate.setId(nextId++);
+        candidates.put(candidate.getId(), candidate);
+        return candidate;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        candidates.remove(id);
+    }
+
+    @Override
+    public boolean update(Candidate candidate) {
+        return candidates.computeIfPresent(candidate.getId(), (id, oldCandidate) -> new Candidate(oldCandidate.getId(),
+                candidate.getName(), candidate.getDescription(), candidate.getCreationDate())) != null;
+    }
+
+    @Override
+    public Optional<Candidate> findById(int id) {
+        return Optional.ofNullable(candidates.get(id));
+    }
+
+    @Override
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
