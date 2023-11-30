dc91fe34b3dfccb27919104a0f4b7e08dcab7f64
PavelChupin <pavelchupin94@yandex.ru>
2023-11-30 18:52:34 +0300

    4. Создание вакансии [#504849]

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -11,13 +11,16 @@ public class Vacancy {
 
     private String description;
 
-    private  LocalDateTime creationDate;
+    private  LocalDateTime creationDate = LocalDateTime.now();
 
     public Vacancy(int id, String title, String description, LocalDateTime creationDate) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.creationDate = creationDate;
+    }
+
+    public Vacancy() {
     }
 
     public int getId() {
