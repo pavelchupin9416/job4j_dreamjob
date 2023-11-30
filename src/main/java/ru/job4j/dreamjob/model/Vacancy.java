2b222cc6c1af6dd65ae59338f29b09225ea6937e
PavelChupin <pavelchupin94@yandex.ru>
2023-11-30 16:59:54 +0300

    5. Список кандидатов. Фикс.

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -54,8 +54,12 @@ public class Vacancy {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Vacancy vacancy = (Vacancy) o;
         return id == vacancy.id;
     }
